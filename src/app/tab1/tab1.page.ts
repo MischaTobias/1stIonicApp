08b329027a51da94e005bd3dde4ae1ca2d3edf4f
MischaTobias <mischa.tobias4b@gmail.com>
2021-10-27 21:40:45 -0600

    Fin sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
 
+  ngOnInit(){
+    let n = 10;
+
+    n = 20;
+
+    n += 30;
+
+    console.log('hola mundo');
+  }
 }
